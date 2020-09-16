26eb3bc4b22c466173e01cfdff7c19aae8068ff1
Youness BENNAJ <youness.bennaj@gmail.com>
2020-09-16 18:52:34 +0200

    add navbar button on mobile

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ import "./style.scss";
 
 const App = () => {
     return (
-        <div className="container">
-            <div className="grid">
-                <div className="text">
-                    <h2>youness bennaj</h2>
-                    <h1>JavaScript & React Developer</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis rem saepe, dolorum tempora magnam dignissimos eaque ipsa veniam aut pariatur dolore reiciendis, autem culpa eos amet qui dolorem at fugiat!</p>
-                    <button>Button text</button>
-                </div>
+        <>
+            <header className="navbar">
+                <a href="#">click</a>
+            </header>
+            <div className="container">
+                <div className="grid">
+                    <div className="text">
+                        <h2>youness bennaj</h2>
+                        <h1>JavaScript & React Developer</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis rem saepe, dolorum tempora magnam dignissimos eaque ipsa veniam aut pariatur dolore reiciendis, autem culpa eos amet qui dolorem at fugiat!</p>
+                        <button>Button text</button>
+                    </div>
 
-                <img className="image" src="https://via.placeholder.com/500x700" alt="" />
+                    <img className="image" src="https://via.placeholder.com/500x700" alt="" />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
