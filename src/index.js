4093f6d4eb0994c92b687d2511e52b28dbb6d3d1
Youness BENNAJ <youness.bennaj@gmail.com>
2020-09-18 19:16:28 +0200

    first version of hero section and navigation

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import wave from "../assets/wave.jpg";
+
 import "./style.scss";
 
 const App = () => {
     return (
         <div className="container">
             <header className="navbar">
-                <a href="#menu" className="navbar__btn">
-                    <span className="burger"></span>
+                <a href="#menu-mobile" className="navbar__toggle-btn">
+                    <span className="navbar__burger-icon"></span>
                 </a>
-                <nav id="menu" className="navigation">
-                    <a href="#" className="navigation__close">close</a>
-                    <ul className="navigation__list">
+                <nav id="menu-mobile" className="navigation-mobile">
+                    <a href="#" className="navigation-mobile__close">close</a>
+                    <ul className="navigation-mobile__list">
                         <li><a href="#about">about</a></li>
                         <li><a href="#portfolio">portfolio</a></li>
                         <li><a href="#contact">contact</a></li>
                     </ul>
                 </nav>
+                <nav className="navigation-desktop">
+                    <ul className="navigation-desktop__list">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
             </header>
-            <div className="main">
-                <div className="grid">
-                    <div className="text">
-                        <h2>👋, I'm Youness Bennaj</h2>
-                        <h1>A <span className="blue">JavaScript</span> & <span className="blue">React</span> Developer</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis rem saepe, dolorum tempora magnam dignissimos eaque ipsa veniam aut pariatur dolore reiciendis, autem culpa eos amet qui dolorem at fugiat!</p>
-                        <button>Get in touch 😉</button>
+            <main className="hero">
+                <div className="hero__grid">
+                    <div className="hero__text">
+                        <h1 className="hero__title">👋, I'm Youness. <span className="blue">JavaScript</span> & <span className="blue">React</span> Developer.</h1>
+                        <p className="hero__paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis rem saepe, dolorum tempora magnam dignissimos eaque ipsa veniam aut pariatur dolore reiciendis, autem culpa eos amet qui dolorem at fugiat!</p>
+                        <button className="hero__btn">Get in touch</button>
                     </div>
-                    <img className="image" src="https://via.placeholder.com/500x700" alt="" />
+                    <h2>Portfolio</h2>
+                    <img className="hero__image" src={wave} alt="" />
                 </div>
-            </div>
+            </main>
         </div>
     );
 }
