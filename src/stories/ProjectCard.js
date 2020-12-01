66e326529ed8a583588a273a0fda22f84d51d384
Youness BENNAJ <youness.bennaj@gmail.com>
2020-12-01 19:54:48 +0100

    add project card component

--- src/stories/ProjectCard.js
+++ src/stories/ProjectCard.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+import Button from "./Button";
+
 const ProjectCard = () => {
     return (
-        <div>
-            <h2>title</h2>
+        <div className="project-card">
+            <div className="project-card__image">
+                <img src="https://source.unsplash.com/random" alt="" />
+            </div>
+
+            <div className="project-card__tags">
+                <span>#HTML</span>
+                <span>#CSS</span>
+                <span>#responsive</span>
+            </div>
+            <h2 className="project-card__title">title</h2>
+            <p className="project-card__description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor at veniam nisi nostrum corrupti excepturi blanditiis magni nesciunt.</p>
+            <div className="button-group">
+                <Button size="medium" color="primary" label="demo" />
+                <Button size="medium" label="code" />
+            </div>
         </div>
     );
 }
